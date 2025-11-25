044966158abe244e5fade969b801109639e7837e
Yaroslav Mazin <ya.bigmazzy@gmail.com>
2025-11-25 14:12:53 +0300

    fast fix

--- internal/service/team_service.go
+++ internal/service/team_service.go
@@ -25,11 +25,9 @@ func (s *TeamService) AddTeam(ctx context.Context, team *models.Team) error {
 	if team.TeamName == "" {
 		return errors.New("Empty Name") // TODO добавить ошибку
 	}
-	// Репозиторий сам сделает upsert + транзакцию
 	return s.teamRepo.UpsertTeam(ctx, team)
 }
 
-// GetTeam — /team/get
 func (s *TeamService) GetTeam(ctx context.Context, teamName string) (*models.Team, error) {
 	if teamName == "" {
 		return nil, errors.New("Empty Name") // TODO добавить ошибку
