3b4e3c0d822f4726df338f933051517510b0d3c9
Yaroslav Mazin <ya.bigmazzy@gmail.com>
2025-11-25 14:14:16 +0300

    uncapitalized error strings

--- internal/service/team_service.go
+++ internal/service/team_service.go
@@ -23,14 +23,14 @@ func NewTeamService(teamRepo TeamRepository) *TeamService {
 
 func (s *TeamService) AddTeam(ctx context.Context, team *models.Team) error {
 	if team.TeamName == "" {
-		return errors.New("Empty Name") // TODO добавить ошибку
+		return errors.New("empty name") // TODO добавить ошибку
 	}
 	return s.teamRepo.UpsertTeam(ctx, team)
 }
 
 func (s *TeamService) GetTeam(ctx context.Context, teamName string) (*models.Team, error) {
 	if teamName == "" {
-		return nil, errors.New("Empty Name") // TODO добавить ошибку
+		return nil, errors.New("empty name") // TODO добавить ошибку
 	}
 	return s.teamRepo.GetTeamByName(ctx, teamName)
 }
